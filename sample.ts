0f614b6c305e469af0b0c9aeba0e89d51e8a1a51
Juan Larrambebere <juanlarrambebere@192.168.0.104>
2022-11-09 11:49:37 -0300

    Multiple stacks, one for production, one for development

---
+++ sample.ts
@@ -0,0 +1,12 @@
+import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+
+export const handler = async (
+  event: APIGatewayEvent
+): Promise<APIGatewayProxyResult> => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hi from lambda!" }),
+  };
+
+  return response;
+};
